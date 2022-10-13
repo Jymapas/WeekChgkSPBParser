faac45d9bc4a45c12938a70c398436ad254d4f78
Александр Петраков <jymapas@yandex.ru>
2022-10-13 10:26:17 +0300

    заплатка

--- WeekChgkSPBParser/Constants.cs
+++ WeekChgkSPBParser/Constants.cs
@@ -2,11 +2,16 @@
 {
     internal record class Constants
     {
-        internal const string TgToken = $"";
+        internal static readonly string TgToken = GetFromTxt("TgToken.txt");
         internal const string LJUrl = "https://chgk-spb.livejournal.com/2596838.html";
         internal const int ChatId = -635211124;
         internal const string ChannelId = "@sapamyJ";
         internal const string TxtAnnouncePath = @"C:\\Users\petrakov_as\Desktop\anounsment.txt";
         internal const string TgHead = "Продолжаем вести список синхронов в Санкт-Петербурге.";
+        private static string GetFromTxt(string path)
+        {
+            using StreamReader sr = new FileInfo(path).OpenText();
+            return sr.ReadToEnd();
+        }
     }
 }
