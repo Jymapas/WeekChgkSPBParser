9599b26bfa94789d8f880ac716258209f3990777
Jymapas <Jymapas@yandex.ru>
2022-10-21 17:00:44 +0300

    Пока что пробная попытка записать это, фиксирую, чтобы дома не продолбать.

--- WeekChgkSPBParser/Constants/Id.cs
+++ WeekChgkSPBParser/Constants/Id.cs
@@ -1,8 +1,24 @@
-﻿namespace WeekChgkSPBParser.Constants
+﻿using WeekChgkSPBParser.API;
+
+namespace WeekChgkSPBParser.Constants
 {
     internal record class Id
     {
         internal const int Chat = -635211124;
         internal const string Channel = "@sapamyJ";
+        private static List<int> Admins
+        {
+            get
+            {
+                string allText = GetFromTxtHelper.GetFromTxt(Paths.AdminIds);
+                foreach (string line in allText.Split(Environment.NewLine))
+                {
+                    int.TryParse(line, out int id);
+                    if (id != 0)
+                        Admins.Add(id);
+                }
+                return Admins;
+            }
+        }
     }
 }
--- WeekChgkSPBParser/Constants/Paths.cs
+++ WeekChgkSPBParser/Constants/Paths.cs
@@ -4,6 +4,7 @@
     {
         internal const string TxtAnnounce = @"C:\\Users\petrakov_as\Desktop\anounsment.txt";
         internal const string TgToken = @"TgToken.txt";
+        internal const string AdminIds = @"IDs.txt";
         internal const string LJUrl = @"https://chgk-spb.livejournal.com/2596838.html";
     }
 }
